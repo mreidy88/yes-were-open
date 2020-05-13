3b96832efed7805c459876dc669fb662d01de9f0
Mike Reidy <mreidy88@gmail.com>
2020-05-13 10:15:23 -0400

    Auth complete

--- yes_were_open/db/seeds.rb
+++ yes_were_open/db/seeds.rb
@@ -5,3 +5,140 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category.create (
+    [
+        {
+        category: "American" 
+        },
+        {
+        category: "Asian"
+        },
+        {
+        category: "BBQ"
+        },
+        {
+        category: "Bar"
+        },
+        {    
+        category: "Bagels"
+        },
+           {           
+        category: "Bakery"
+        },
+           {
+        category: "Bowls"
+        },
+           {
+        category: "Breakfast"
+        },
+           {
+        category: "Cafe"
+        },
+           {
+        category: "Cakes"
+        },
+           {
+        category: "Caribbean"
+        },
+           {
+        category: "Chicken"
+        },
+           {
+        category: "Coffee and Tea"
+        },
+           {
+        category: "Crepes"
+        },
+           {
+        category: "Deli"
+        },
+           {
+        category: "Dessert"
+        },
+           {
+        category: "Diner"
+        },
+           {
+        category: "Fresh Fruits"
+        },
+           {
+        category: "Grill"
+        },
+           {
+        category: "Hamburgers"
+        },
+           {
+        category: "Healthy"
+        },
+           {
+        category: "Latin American"
+        },
+           {
+        category: "Mediterranean"
+        },
+           {
+        category: "Mexican"
+        },
+           {
+        category: "Noodles"
+        },
+           {
+        category: "Pasta"
+        },
+           {
+        category: "Pizza"
+        },
+           {
+        category: "Ribs"
+        },
+           {
+        category: "Salads"
+        },
+           {
+        category: "Sandwiches"
+        },
+           {
+        category: "Seafood"
+        },
+           {
+        category: "Shakes"
+        },
+           {
+        category: "Smoothies"
+        },
+           {
+        category: "Juices"
+        },
+           {
+        category: "Snacks"
+        },
+           {
+        category: "Soup" 
+        },
+           {
+        category: "Steak" 
+        },
+           {
+        category: "Subs"
+        },
+           {
+        category: "Tacos" 
+        },
+           {
+        category: "Vegan"
+        },
+           {
+        category: "Vegetarian"
+        },
+           {
+        category: "Waffles" 
+        },
+           {
+        category: "Wings"
+        },
+           {
+        category: "Wraps"
+        }
+    ]
+)
